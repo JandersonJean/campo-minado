cf9f008d5ad670fb69f667252a359d8b5d8deb46
Janderson Jean <jandersonjean@gmail.com>
2025-02-14 06:33:16 -0300

    Classe Tabuleiro - Criando Atributo, Construtor e Metodo

--- src/com/janderson/modelo/Tabuleiro.java
+++ src/com/janderson/modelo/Tabuleiro.java
@@ -1,5 +1,48 @@
 package com.janderson.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
 
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linha, int colunas, int minas) {
+		this.linhas = linha;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}
+		}
+		
+	}
+	
+	private void associarVizinhos() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+	
+	private void sortearMinas() {
+		
+	}
+
+
+	
+	
 }
